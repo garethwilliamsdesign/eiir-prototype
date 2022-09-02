0712db52dee74bb51abdbc0d20b22836a23a8731
garethwilliamsdesign <hello@garethwilliamsdesign.co.uk>
2022-09-02 16:42:52 +0100

    Update of Citizen v1

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/v1/index', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var search = req.session.data['search']
+  
+    // Check whether the variable matches a condition
+    if (search == "individual"){
+      // Send user to next page
+      res.redirect('/v1/individual-search-page')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/v1/trading-name-search-page')
+    }
+  
+  })
+
 module.exports = router
