f9106d6fc02e25843a2009f9a78c90d2c67e8c1d
garethwilliamsdesign <hello@garethwilliamsdesign.co.uk>
2022-10-04 17:06:18 +0100

    Update of subscriber profile screens in Admin v1

--- app/routes.js
+++ app/routes.js
@@ -20,4 +20,20 @@ router.post('/v1/index', function (req, res) {
   
   })
 
+router.post('/admin/v1/subscriber-profile-page-1-remove', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var remove = req.session.data['remove']
+
+  // Check whether the variable matches a condition
+  if (remove == "yes"){
+    // Send user to next page
+    res.redirect('/admin/v1/subscriber-profile-page-1')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/admin/v1/subscriber-profile-page-1-added')
+  }
+
+})
+
 module.exports = router
